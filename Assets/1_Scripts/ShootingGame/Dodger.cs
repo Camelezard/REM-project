68ed2f0c09e779ccef95a2f924d00d779545d30b
r.moal <r.moal@student.isartdigital.com>
2026-02-05 15:54:47 +0100

    ADD : Scene + characters

---
+++ Assets/1_Scripts/ShootingGame/Dodger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Dodger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected float speed = 10f;
+    protected Vector2 velocity;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected virtual void Movement()
+    {
+
+    }
+}
