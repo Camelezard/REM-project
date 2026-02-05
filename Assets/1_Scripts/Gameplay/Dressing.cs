480da2e8f4e9893a0cb2a0a40dd440632a602113
Daniel <d.degott@student.isartdigital.com>
2026-02-05 11:40:27 +0100

    Setting up the scene and adding the placeholder

---
+++ Assets/1_Scripts/Gameplay/Dressing.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dressing : MonoBehaviour
+{
+    [Header("Closes")]
+    [SerializeField] private List<GameObject> _Pants;
+    [SerializeField] private List<GameObject> _Choses;
+    [SerializeField] private List<GameObject> _DressTop;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
