79dc2e6166a8c06672137d8a698b66d23c331260
r.moal <r.moal@student.isartdigital.com>
2026-02-06 15:38:05 +0100

    ADD : Timer

--- Assets/1_Scripts/ShootingGame/Bullet.cs
+++ Assets/1_Scripts/ShootingGame/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour
 
     void Start()
     {
-        Destroy(gameObject, 3f);
+        Destroy(gameObject, 2f);
     }
 
     // Update is called once per frame
--- Assets/1_Scripts/ShootingGame/ShootingMinigameManager.cs
+++ Assets/1_Scripts/ShootingGame/ShootingMinigameManager.cs
@@ -1,16 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class ShootingMinigameManager : MonoBehaviour
 {
+    public float gameDuration = 45f;
+    private float _ElapsedTime = 45f;
+
+    public TextMeshProUGUI timerText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _ElapsedTime = gameDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _ElapsedTime -= Time.deltaTime;
+        timerText.text = _ElapsedTime.ToString("F0");
     }
 }
