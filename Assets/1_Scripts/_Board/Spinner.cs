98d1a6f4999ab97c103442c95b733f4ab6495a73
r.moal <r.moal@student.isartdigital.com>
2026-02-09 17:02:08 +0100

    ADD : Spinner.cs + Spinner in UI

---
+++ Assets/1_Scripts/_Board/Spinner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public int minMovementPoint = 1;
+    public int maxMovementPoint = 4;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpinWheel()
+    {
+
+    }
+}
