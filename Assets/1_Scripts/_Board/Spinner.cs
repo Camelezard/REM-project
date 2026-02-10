5672635a7cc75a915ae057b095c25a43612a2157
r.moal <r.moal@student.isartdigital.com>
2026-02-10 17:08:59 +0100

    ADD : Wheel Animation, need work.

--- Assets/1_Scripts/_Board/Spinner.cs
+++ Assets/1_Scripts/_Board/Spinner.cs
@@ -1,20 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
+    [Header("Gameplay Related")]
     public int minMovementPoint = 1;
     public int maxMovementPoint = 4;
 
-    public Transform numberParent;
+    [Header("Number Related")]
+    public RectTransform wheel;
     public int numberFontSize = 100;
     public Color numberFontColor = Color.black;
     public float numberWheelRadius;
+
+    [Header("Wheel Anim Related")]
+    public AnimationCurve spinAnimCurveStart;
+    public AnimationCurve spinAnimCurveEnd;
+    public float startingSpinDuration;
+    public float endingSpinDuration;
 
 
     void Start()
     {
         GenerateNumberOnWheel();
+        SpinWheel();
     }
 
     // Update is called once per frame
@@ -42,7 +53,7 @@ public class Spinner : MonoBehaviour
             lNumberText.color = numberFontColor;
             lNumberText.alignment = TextAlignmentOptions.Center;
 
-            lNumberObject.transform.SetParent(numberParent, false);
+            lNumberObject.transform.SetParent(wheel, false);
 
             lAngle = (90f - (i * 360f / maxMovementPoint)) * Mathf.Deg2Rad;
 
@@ -58,6 +69,29 @@ public class Spinner : MonoBehaviour
 
     public void SpinWheel()
     {
+        int lRandomMovementPoint = Random.Range(minMovementPoint, maxMovementPoint);
+        StartCoroutine(WheelAnimation());
+    }
 
+    private IEnumerator WheelAnimation()
+    {
+        float lRatio = 0;
+        float lElaspedTime = 0;
+
+        Quaternion lStartRot = wheel.localRotation;
+        Quaternion lEndRot = wheel.localRotation * Quaternion.Euler(0, 0, 360f * 6f);
+
+        while (lElaspedTime < startingSpinDuration)
+        {
+            lElaspedTime += Time.deltaTime;
+            lRatio = lElaspedTime / startingSpinDuration;
+
+            wheel.localRotation = Quaternion.Slerp(lStartRot, lEndRot, spinAnimCurveStart.Evaluate(lRatio));
+            yield return null;
+        }
+
+        wheel.localRotation = lEndRot;
+
+        yield return null;
     }
 }
