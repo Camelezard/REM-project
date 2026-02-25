9ee05e96f35c24e3a1f6ffe623971594bec9dfe6
Daniel <d.degott@student.isartdigital.com>
2026-02-25 09:10:11 +0100

    fix minigames

--- Assets/1_Scripts/Managers/GameManager.cs
+++ Assets/1_Scripts/Managers/GameManager.cs
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour
     private const string MINIGAME_DRESSING = "Habillage";
     private const string MINIGAME_DANSING = "RetenirLesGestes";
 
+    private string CurrentMinigameName;
+
     public static GameManager instance { get; private set; }
     public List<Player> _PlayersList { get; private set; }
 
@@ -66,7 +68,7 @@ public class GameManager : MonoBehaviour
 
     private void OnDisable()
     {
-        instance = null;
+        //instance = null;
     }
 
     public void CreatePlayers(List<Player> lPlayerNumber)
@@ -77,7 +79,11 @@ public class GameManager : MonoBehaviour
 
     //Get turn
     public Player GetCurrentPlayerTurn() => _PlayersList[CurrentPlayer];
+    public Player GetPlayerOne() => _PlayersList[0];
+    public Player GetPlayerTow() => _PlayersList[1];
+    public Player GetPlayerInList(int pIndex) => _PlayersList[pIndex - 1];
     public int GetCurrentPlayerIndex() => CurrentPlayer;
+
 
 
     //Tunrn Management
@@ -102,12 +108,29 @@ public class GameManager : MonoBehaviour
 
     public void StartMinigame(TypOfMinigame pTyp)
     {
-        SceneTransitionanager.instance.LoadSingle(GetSceneNamWithEnum(pTyp));
+        CurrentMinigameName = GetSceneNamWithEnum(pTyp);
+
+        SceneManager.LoadScene(CurrentMinigameName, LoadSceneMode.Additive);
+
+        Scene minigame = SceneManager.GetSceneByName(CurrentMinigameName);
+        SceneManager.SetActiveScene(minigame);
     }
 
     public void WinGame(Player pPlayer)
     {
-        SceneTransitionanager.instance.LoadAdditive(MINIGAME_TEST);
+        if (CurrentMinigameName == null)
+        {
+            Debug.Log("CurrentMinigameName not set");
+            return;
+        }
+
+        SceneManager.UnloadSceneAsync(CurrentMinigameName);
+        CurrentMinigameName = null;
+
+        Scene board = SceneManager.GetSceneByName(MAIN_BOARD_SCENE_NAME);
+        SceneManager.SetActiveScene(board);
+
+        NextPlayerTurn();
     }
 
     // private void ResetGameState()
