e2ced61fb96fa72b40e422a4107430d80d1e64ae
r.moal <r.moal@student.isartdigital.com>
2026-03-26 15:55:59 +0100

    ADD : Reworked ShootingMinigame Input

--- Assets/1_Scripts/ShootingGame/Dodger.cs
+++ Assets/1_Scripts/ShootingGame/Dodger.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class Dodger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    protected float m_speed = 5f;
+    [SerializeField] protected float m_speed = 5f;
     protected Vector2 m_velocity;
 
-    protected float m_screenWidth;
+    [SerializeField] protected float m_horizontalzonePercent = 0.5f;
+    [SerializeField] protected float m_verticalZonePErcent = 0.5f;
+    [SerializeField] protected PlayerZone m_PlayerZone = PlayerZone.Bottom;
 
-    protected InputAction m_movementAction;
-    [SerializeField] protected string m_movementActionPath;
     protected virtual void Start()
     {
-        m_movementAction = InputSystem.actions.FindAction(m_movementActionPath);
+        EnhancedTouchSupport.Enable();
     }
 
     // Update is called once per frame
@@ -25,21 +27,69 @@ public class Dodger : MonoBehaviour
 
     protected virtual void Movement()
     {
-        //// Vérifie s'il y a des touches actives
-        //if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
-        //{
-        //    // Récupère la position du toucher
-        //    Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        foreach (var touch in Touch.activeTouches)
+        {
+            Vector2 lTouchPos = touch.screenPosition;
 
-        //    // Détermine si c'est le côté gauche ou droit de l'écran
-        //    float screenWidth = Screen.width;
+            if (m_PlayerZone == PlayerZone.Bottom)
+            {
+                if (lTouchPos.x < Screen.width * m_horizontalzonePercent && lTouchPos.y < Screen.height * m_verticalZonePErcent)
+                {
+                    // Bottom left
+                    if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary || touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
+                    {
+                        m_velocity = Vector2.left;
+                    }
+                    else
+                    {
+                        m_velocity = Vector2.zero;
+                    }
+                }
 
+                if (lTouchPos.x > Screen.width * m_horizontalzonePercent && lTouchPos.y < Screen.height * m_verticalZonePErcent)
+                {
+                    // Bottom right
+                    if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary || touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
+                    {
+                        m_velocity = Vector2.right;
+                    }
+                    else
+                    {
+                        m_velocity = Vector2.zero;
+                    }
+                }
+            }
 
-        //}
-        m_velocity.x = m_movementAction.ReadValue<float>();
-        Debug.Log(m_velocity);
-        transform.position += new Vector3(m_velocity.x, 0, 0) * m_speed * Time.deltaTime;
+            else if (m_PlayerZone == PlayerZone.Top)
+            {
+                if (lTouchPos.x > Screen.width * m_horizontalzonePercent && lTouchPos.y > Screen.height * m_verticalZonePErcent)
+                {
+                    // Top right
+                    if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary || touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
+                    {
+                        m_velocity = Vector2.right;
+                    }
+                    else
+                    {
+                        m_velocity = Vector2.zero;
+                    }
+                }
 
-        
+                if (lTouchPos.x < Screen.width * m_horizontalzonePercent && lTouchPos.y > Screen.height * m_verticalZonePErcent)
+                {
+                    // Top left
+                    if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary || touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
+                    {
+                        m_velocity = Vector2.left;
+                    }
+                    else
+                    {
+                        m_velocity = Vector2.zero;
+                    }
+                }
+            }
+        }
+
+        transform.position += new Vector3(m_velocity.x, m_velocity.y, 0) * m_speed * Time.deltaTime;
     }
 }
