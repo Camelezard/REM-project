99ac902e922918f6fa2c57f5b775ab945d998863
r.moal <r.moal@student.isartdigital.com>
2026-03-30 15:59:06 +0200

    ADD : SoundManager Singleton

---
+++ Assets/1_Scripts/Managers/SoundManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+}
