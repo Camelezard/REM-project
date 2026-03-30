d07c9f7864432d880eae8e2e68c7294e4289f5b2
r.moal <r.moal@student.isartdigital.com>
2026-03-30 18:06:06 +0200

    ADD : Musics files, AudioMixer, Functions

--- Assets/1_Scripts/Managers/SoundManager.cs
+++ Assets/1_Scripts/Managers/SoundManager.cs
@@ -1,9 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
+public enum Musics
+{
+    CalmBoard,
+    CalmDance,
+    CalmLove,
+    CalmPeps,
+    Energic,
+    LoveTunnel,
+    TVMusic
+}
+
+public enum SFX
+{
+    None
+}
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
+    [Header("SFX Lists")]
+    [SerializeField] private List<AudioResource> _MusicResources = new List<AudioResource>();
+    [SerializeField] private List<AudioResource> _SFXResources = new List<AudioResource>();
+
+    [Header("AudioSources")]
+    [SerializeField] private AudioSource _MusicAudioSource;
+    [SerializeField] private AudioSource _SFXAudioSource;
     private void Awake()
     {
         if (Instance == null)
@@ -15,8 +41,20 @@ public class SoundManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+        DontDestroyOnLoad(gameObject);
 
+    }
 
+    public void PlayMusic(Musics pMusic)
+    {
+        _MusicAudioSource.resource = _MusicResources[(int)pMusic];
+        _MusicAudioSource.Play();
+    }
+
+    public void PlaySFX(SFX pSFX)
+    {
+        _SFXAudioSource.resource = _SFXResources[(int)pSFX];
+        _SFXAudioSource.Play();
     }
 
     private void OnDestroy()
